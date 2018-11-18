95ff3bdb1743587600df9c0c363900291d84df71
gkulatskiy <40183523+gkulatskiy@users.noreply.github.com>
2018-11-18 19:29:59 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace Temperature_scales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 134);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(240, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 304);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(240, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(262, 381);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(123, 34);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Рассчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "_";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 310);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(18, 20);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "_";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 20);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Выберите размерность";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(20, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 20);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Напишите значение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 442);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Температурные шкалы";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Temperature_scales
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Создаем список
+            comboBox1.Items.Add("Градус Цельсия(C)");
+            comboBox1.Items.Add("Градус Фаренгейта (F)");
+            comboBox1.Items.Add("Кельвин (K)");
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           //Объявляем переменные
+            double C, F, K;
+            bool fl1 = false;
+            bool fl2 = false;
+            bool fl3 = false;
+            //Делаем проверку, чтоб все поля были заполнены
+            if (textBox1.Text == "" || comboBox1.Text == "")
+            {
+                MessageBox.Show("Не все данные введены!");
+
+            }
+            else
+            {
+                switch ((short)comboBox1.SelectedIndex)
+                {
+                    //Изменяем флаги при разных выборах списка
+                    case 0:
+                        {
+                            fl1 = true;
+                            break;
+                        }
+                    case 1:
+                        {
+                            fl2 = true;
+                            break;
+                        }
+                    case 2:
+                        {
+                            fl3 = true;
+                            break;
+                        }
+                    default:
+                        {
+                            fl1 = false;
+                            fl2 = false;
+                            fl3 = false;
+                            break;
+                        }
+                }
+                //Проверка
+                if (fl1 == true && fl2 == false && fl3 == false)
+                {
+                    // Проверка на правильность введения значения в поле
+                    try
+                    {
+                        C = double.Parse(textBox1.Text);//Считываем "С"
+                        F = ((C * 9 / 5) + 32);//Считаем "F"
+                        K = (C + 273.2);//Считаем "K
+                        label1.Text = F.ToString("f1") + " " + "F";//Выводим "F"
+                        label2.Text = K.ToString("f1") + " " + "K";//Выводим "К"
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Некорректный ввод данных!");
+                    }
+                   
+                   
+
+                }
+                //Проверка
+                if (fl1 == false && fl2 == true && fl3 == false)
+                {
+                    // Проверка на правильность введения значения в поле
+                    try
+                    {
+                        F = double.Parse(textBox1.Text);
+                        C = ((F - 32) * 5 / 9);
+                        K = C + 273.2;
+                        label1.Text = C.ToString("f1") + " " + "C";//Выводим "С"
+                        label2.Text = K.ToString("f1") + " " + "K";//Выводим "К"
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Некорректный ввод данных!");
+                    }
+                   
+                }
+                //Проверка
+                if (fl1 == false && fl2 == false && fl3 == true)
+                {
+                    // Проверка на правильность введения значения в поле
+                    try
+                    {
+                        K = double.Parse(textBox1.Text);
+                        C = K - 273.2;
+                        F = F = ((C * 9 / 5) + 32);
+                        label1.Text = C.ToString("f1") + " " + "C";//Выводим "С"
+                        label2.Text = F.ToString("f1") + " " + "F";//Выводим "F"
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Некорректный ввод данных!");
+                    }
+                }
+            }
+           
+
+
+
+
+            }
+        }
+     
+    }
+
